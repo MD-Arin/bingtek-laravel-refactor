0824f8c21d99d60dab542038613fc96bbcd45a1a
MD-Arin <graffymint@gmail.com>
2016-12-26 21:08:44 +0300

    Modified the Home and About page

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -18,16 +18,16 @@
     <h3 class="text-center"></h3>
     <hr class="underline">
     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <img src="" class="img-thumbnail"/>
+      <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail"/>
     </div>
     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <h4></h4>
-      <h4></h4>
+      <h4>Founder: Founder's Name</h4>
+      <h4>Year: Founding Date</h4>
       <hr>
       <h4>The Story: </h4>
       <hr>
       <p>
-
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
       </p>
     </div>
   </div>
@@ -48,14 +48,34 @@
 
           <!-- Wrapper for slides -->
           <div class="carousel-inner" role="listbox">
-            <div class="item active">
+            <div class="item active carousel-height">
               <div class="row">
-                <img src="">
+                <img src="{{ URL::to('img/placeholders/placeholder-2.jpg') }}">
+                <div class="carousel-caption-right">
+                  <h4>Year: 2005</h4>
+                  <h5>Accomplishment: Best Networkers in Mombasa</h5>
+                  <hr>
+                  <h4>The Story: </h4>
+                  <hr>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                  </p>
+                </div>
               </div>
             </div>
-            <div class="item">
+            <div class="item carousel-height">
               <div class="row">
-                <img src="">
+                <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}">
+                <div class="carousel-caption-right">
+                  <h4>Year: 2006</h4>
+                  <h5>Accomplishment: Best Internet Provider in Mombasa</h5>
+                  <hr>
+                  <h4>The Story: </h4>
+                  <hr>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -72,33 +92,11 @@
         </div>
       </div>
     </div>
-
-      <br>
-      <br>
-
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <h4></h4>
-      <h4></h4>
-      <hr>
-      <h4>The Story: </h4>
-      <hr>
-      <p>
-
-      </p>
-    </div>
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-      <h4></h4>
-      <h4></h4>
-      <hr>
-      <h4>The Story: </h4>
-      <hr>
-      <p>
-
-      </p>
-    </div>
   </div>
+
   <br>
   <br>
+
   <div class="text-center">
     <hr>
     <h1>Testimonials</h1>
@@ -106,44 +104,44 @@
   </div>
   <div class="row">
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <img src="" class="img-thumbnail" />
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}" class="img-thumbnail" />
       <p class="blockquote-reverse">
-
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
       </p>
       <hr>
-      <h5 class="blockquote"><i>Name: </i></h5>
-      <h5 class="blockquote"><i>Position: </i> </h5>
-      <h5 class="blockquote"><i>Company: </i></h5>
+      <h5 class="blockquote"><i>Name: John Doe</i></h5>
+      <h5 class="blockquote"><i>Position: Procurement</i> </h5>
+      <h5 class="blockquote"><i>Company: Satisfied Ltd</i></h5>
     </div>
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <img src="" class="img-thumbnail" />
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}" class="img-thumbnail" />
       <p class="blockquote-reverse">
-
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
       </p>
       <hr>
-      <h5 class="blockquote"><i>Name: </i></h5>
-      <h5 class="blockquote"><i>Position: </i> </h5>
-      <h5 class="blockquote"><i>Company: </i></h5>
+      <h5 class="blockquote"><i>Name: Jane Doe</i></h5>
+      <h5 class="blockquote"><i>Position: CEO</i> </h5>
+      <h5 class="blockquote"><i>Company: Logistics Ltd</i></h5>
     </div>
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <img src="" class="img-thumbnail" />
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}" class="img-thumbnail" />
       <p class="blockquote-reverse">
-
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
       </p>
       <hr>
-      <h5 class="blockquote"><i>Name: </i></h5>
-      <h5 class="blockquote"><i>Position: </i> </h5>
-      <h5 class="blockquote"><i>Company: </i></h5>
+      <h5 class="blockquote"><i>Name: John Doe</i></h5>
+      <h5 class="blockquote"><i>Position: Marketing</i> </h5>
+      <h5 class="blockquote"><i>Company: Designs Ltd</i></h5>
     </div>
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <img src="" class="img-thumbnail" />
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}" class="img-thumbnail" />
       <p class="blockquote-reverse">
-
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
       </p>
       <hr>
-      <h5 class="blockquote"><i>Name: </i></h5>
-      <h5 class="blockquote"><i>Position:  </i> </h5>
-      <h5 class="blockquote"><i>Company: </i></h5>
+      <h5 class="blockquote"><i>Name: Jane Doe</i></h5>
+      <h5 class="blockquote"><i>Position:  Sales</i> </h5>
+      <h5 class="blockquote"><i>Company: Merchant Ltd</i></h5>
     </div>
   </div>
 
@@ -165,24 +163,42 @@
       <div class="carousel-inner" role="listbox">
         <div class="item active">
           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img src="" class="img-thumbnail">
+            <img src="{{ URL::to('img/placeholders/placeholder-0.jpg') }}" class="img-thumbnail">
+            <div class="carousel-caption">
+              Partner 1
+            </div>
           </div>
           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img src="" class="img-thumbnail">
+            <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail">
+            <div class="carousel-caption">
+              Partner 2
+            </div>
           </div>
           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img src="" class="img-thumbnail">
+            <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail">
+            <div class="carousel-caption">
+              Partner 3
+            </div>
           </div>
         </div>
         <div class="item">
           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img src="" class="img-thumbnail">
+            <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail">
+            <div class="carousel-caption">
+              Partner 4
+            </div>
           </div>
           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img src="" class="img-thumbnail">
+            <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail">
+            <div class="carousel-caption">
+              Partner 5
+            </div>
           </div>
           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <img src="" class="img-thumbnail">
+            <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail">
+            <div class="carousel-caption">
+              Partner 6
+            </div>
           </div>
         </div>
         </div>
@@ -207,22 +223,22 @@
   </div>
   <div class="row">
     <div class="col=xs-4 col-sm-4 col-md-4 col-lg-4">
-      <img src=""
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}"
       class="center-block img-circle img-responsive">
-      <h3 class="text-center"></h3>
-      <p class="text-center"></p>
+      <h3 class="text-center">John Doe</h3>
+      <p class="text-center">CEO</p>
     </div>
     <div class="col=xs-4 col-sm-4 col-md-4 col-lg-4">
-      <img src=""
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}"
       class="center-block img-circle img-responsive">
-      <h3 class="text-center"></h3>
-      <p class="text-center"></p>
+      <h3 class="text-center">Jane Doe</h3>
+      <p class="text-center">CFO</p>
     </div>
     <div class="col=xs-4 col-sm-4 col-md-4 col-lg-4">
-      <img src=""
+      <img src="{{ URL::to('img/placeholders/user-placeholder.png') }}"
       class="center-block img-circle img-responsive">
-      <h3 class="text-center"></h3>
-      <p class="text-center"></p>
+      <h3 class="text-center">John Doe</h3>
+      <p class="text-center">COO</p>
     </div>
   </div>
 </div>
