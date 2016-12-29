ce489ca44621884564b9fe5ad65fe500fe4de896
MD-Arin <graffymint@gmail.com>
2016-12-29 03:12:28 +0300

    Finished outlaying the front end

--- resources/views/main/fourth_ps.blade.php
+++ resources/views/main/fourth_ps.blade.php
@@ -13,42 +13,54 @@
 
 @section('content')
   <div class="container-fluid">
-    <div class="row">
-      <div class="well-lg">
-        <img src=""  width="100%" height="500px"/>
-        <div class="jumbotron text-center">
+    <div class="row carousel-height">
+      <div class="cover-image carousel-height" style="background: url('{{ URL::to('img/placeholders/placeholder-1.jpg') }}'); width: 100%;">
+        <div class="jumbotron text-right" style="padding-top: 10%;">
+          <h1>Office Partitioning</h1>
           <p>
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
           </p>
         </div>
       </div>
     </div>
 
+    <hr>
+
     <div class="row">
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <img src="" class="img-thumbnail" />
+        <img src="{{ URL::to('img/placeholders/placeholder-2.jpg') }}" class="img-thumbnail" />
       </div>
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <h4></h4>
+        <h4>Smart and Organized</h4>
+        <p>
+          Whether closed or open offices, our office designs are unmatched.
+        </p>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
         </ul>
       </div>
     </div>
     <hr>
     <div class="row">
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <h4></h4>
+        <h4>All Office Types</h4>
         <p>
-
+          From big offices to small offices, we offer the best office solutions for all types of offices
         </p>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</li>
+        </ul>
       </div>
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <img src="" class="img-thumbnail" />
+        <img src="{{ URL::to('img/placeholders/placeholder-0.jpg') }}" class="img-thumbnail" />
       </div>
     </div>
     <hr>
@@ -56,32 +68,36 @@
     <div class="row">
       <div class="container">
         <div class="jumbotron">
-          <h4 class="text-center"></h4>
-          <h5 class="text-center"></h5>
+          <h4 class="text-center">Comfortable Work Places</h4>
+          <h5 class="text-center">Making offices a home away from home</h5>
           <hr>
           <p class="text-center">
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
           </p>
         </div>
       </div>
     </div>
     <hr>
 
-    <div class="row">
-      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <img src="" alt="placeholder-0.jpg" class="img-thumbnail" />
+    <div class="row carousel-height">
+      <div class="cover-image carousel-height" style="background: url('{{ URL::to('img/placeholders/placeholder-0.jpg') }}'); width: 100%;">
+        <div class="text-left" style="padding-top: 15%">
+          <h2>Offices Designed For Interaction</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+          </p>
+        </div>
       </div>
-      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <p>
-
-        </p>
-        <hr>
-        <p>
-
-        </p>
-      </div>
-      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <img src="" alt="placeholder-1.jpg" class="img-thumbnail" />
+    </div>
+    <hr>
+    <div class="row carousel-height">
+      <div class="cover-image carousel-height" style="background: url('{{ URL::to('img/placeholders/placeholder-1.jpg') }}'); width: 100%;">
+        <div class="text-center" style="padding-top: 15%">
+          <h2>Luxury Offices</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+          </p>
+        </div>
       </div>
     </div>
     <hr>
--- resources/views/main/third_ps.blade.php
+++ resources/views/main/third_ps.blade.php
@@ -17,11 +17,11 @@
     <div class="row">
       <div class="container">
         <hr>
-        <h2 class="list-group-item-heading text-center"></h2>
+        <h2 class="list-group-item-heading text-center">Software Development</h2>
         <hr>
         <div class="jumbotron">
           <p class="list-group-item-text text-center">
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
           </p>
         </div>
       </div>
@@ -29,58 +29,88 @@
 
     <div class="row">
       <hr class="underline">
-      <h2 class="text-center"></h2>
+      <h2 class="text-center">Customized Software Solutions</h2>
       <hr class="underline">
     </div>
 
     <div class="row vertical-line-left vertical-line-right">
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <h3></h3>
+          <h3>Software made Specifically for you</h3>
           <p>
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
           </p>
-          <img src="" class="img-thumbnail" />
+          <ul class="list-unstyled">
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          </ul>
+          <img src="{{ URL::to('img/placeholders/placeholder-2.jpg') }}" class="img-thumbnail" />
       </div>
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <img src="" class="img-thumbnail" />
-          <h3></h3>
+        <img src="{{ URL::to('img/placeholders/placeholder-0.jpg') }}" class="img-thumbnail" />
+          <h3>A wide range of softwares</h3>
           <p>
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
           </p>
+          <ul class="list-unstyled">
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          </ul>
       </div>
     </div>
 
     <div class="row">
       <hr class="underline">
-      <h3 class="text-center"></h3>
+      <h3 class="text-center">Software Maintenance</h3>
       <hr class="underline">
     </div>
 
     <div class="row vertical-line-left vertical-line-right">
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <img src="" class="img-thumbnail" />
-          <h3></h3>
+          <img src="{{ URL::to('img/placeholders/placeholder-0.jpg') }}" class="img-thumbnail" />
+          <h3>Quick and Easy Software Installations</h3>
           <p>
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
           </p>
+          <ol class="list-unstyled">
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          </ol>
       </div>
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <h3></h3>
+          <h3>The Latest Software Upgrades</h3>
           <p>
-
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
           </p>
-          <img src="" class="img-thumbnail" />
+          <ol class="list-unstyled">
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          </ol>
+          <img src="{{ URL::to('img/placeholders/placeholder-1.jpg') }}" class="img-thumbnail" />
       </div>
     </div>
 
     <div class="row">
       <div class="jumbotron">
         <hr class="underline">
-        <h3 class="text-center"></h3>
+        <h3 class="text-center">Diverse Software Solutions</h3>
         <hr class="underline">
         <p class="text-center">
-
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
         </p>
+        <ol class="list-unstyled text-center">
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+          <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </li>
+        </ol>
       </div>
     </div>
   </div>
