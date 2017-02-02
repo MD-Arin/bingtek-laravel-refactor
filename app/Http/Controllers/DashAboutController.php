006231dccaa9f0148234d69b14e68d8d8bd93998
md-arin <graffymint@gmail.com>
2017-02-02 07:08:15 +0300

    Added DashAboutController

---
+++ app/Http/Controllers/DashAboutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use App\Content;
+use Illuminate\Support\Facades\Auth;
+use Image;
+
+class DashAboutController extends Controller
+{
+    public function updateHome(Request $request)
+    {
+        if(Auth::check())
+        {
+            $content = new Content;
+            
+            
+        }
+    }
+}
